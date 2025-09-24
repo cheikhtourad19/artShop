fa5687c9a336831aef28b131d14fe18dbe4a38cf
Cheikhtourad19 <elghothvadel@gmail.com>
2025-09-24 21:35:26 +0100

    remove French comments from user model

--- Models/User.js
+++ Models/User.js
@@ -14,14 +14,12 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash du mot de passe avant sauvegarde
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Comparer mot de passe lors du login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
